5d34f70505faa6550f8a572bb1c6ce11f2baae96
Nasreen Shaikh <P2516540@my365.dmu.ac.uk>
2021-03-09 18:14:45 +0000

    Created the collection class - Pressentation layer and properties.

--- AdminSystem/CustomerList.aspx.cs
+++ AdminSystem/CustomerList.aspx.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
-public partial class _1_List : System.Web.UI.Page
+
+public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if(IsPostBack == false)
+        {
+            DisplayCustomers();
+        }
+    }
 
+    void DisplayCustomers()
+    {
+        clsCustomerCollection Customers = new clsCustomerCollection();
+        lstCustomerList.DataSource = Customers.CustomerList;
+        lstCustomerList.DataValueField = "CustomerId";
+        lstCustomerList.DataTextField = "CustomerName";
+        lstCustomerList.DataBind();
     }
 }
---
+++ Testing2/tstCustomerCollection.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            Assert.IsNotNull(AllCustomers);
+        }
+        [TestMethod]
+        public void CustomerListOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.Active = true;
+            TestItem.BillingAddress = "Some billing address LE33DA";
+            TestItem.DateOfBirth = DateTime.Now.Date;
+            TestItem.Email = "some email @ hmail.com";
+            TestItem.Id = 1;
+            TestItem.Name = "Some name";
+            TestItem.Pass = "Some password";
+            TestList.Add(TestItem);
+            AllCustomers.CustomerList = TestList;
+            Assert.AreEqual(AllCustomers.CustomerList, TestList);
+
+
+        }
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.Active = true;
+            TestItem.BillingAddress = "Some billing address LE33DA";
+            TestItem.DateOfBirth = DateTime.Now.Date;
+            TestItem.Email = "Some email @ hmail.com";
+            TestItem.Id = 1;
+            TestItem.Name = "Some name";
+            TestItem.Pass = "Some password";
+            TestList.Add(TestItem);
+            AllCustomers.CustomerList = TestList;
+            Assert.AreEqual(AllCustomers.Count, TestList.Count);
+
+        }
+
+
+        [TestMethod]
+        public void ThisCustomerPropertyOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            clsCustomer TestCustomer = new clsCustomer();
+            TestCustomer.Active = true;
+            TestCustomer.BillingAddress = "Some billing address LE33DA";
+            TestCustomer.DateOfBirth = DateTime.Now.Date;
+            TestCustomer.Email = "some email @ hmail.com";
+            TestCustomer.Id = 1;
+            TestCustomer.Name = "Some name";
+            TestCustomer.Pass = "Some password";
+            AllCustomers.ThisCustomer = TestCustomer;
+            Assert.AreEqual(AllCustomers.ThisCustomer, TestCustomer);
+
+
+        }
+       
+        
+       
+    }
+}
